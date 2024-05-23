d1fa1da2aa418e5783c3b7221272338a77ab5917
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-05-24 01:04:41 +0700

    Create MilkDTO

---
+++ MomMomMilks/DataTransfer/MilkDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransfer
+{
+    public class MilkDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public string UserManual { get; set; }
+        public string Warning { get; set; }
+        public string PreserveInstructions { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public string MilkAge { get; set; }
+        public string Supplier { get; set; }
+    }
+}
