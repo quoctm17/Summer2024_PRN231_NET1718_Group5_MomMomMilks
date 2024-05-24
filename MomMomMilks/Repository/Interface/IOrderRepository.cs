64f756afba03f4dedb776be6e355ac0daddc8476
vsnghi95 <vsnghi95@gmail.com>
2024-05-25 03:10:54 +0700

    implement api getAllOrder

---
+++ MomMomMilks/Repository/Interface/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IOrderRepository
+    {
+        Task<List<OrderDTO>> GetAllOrders();
+    }
+}
