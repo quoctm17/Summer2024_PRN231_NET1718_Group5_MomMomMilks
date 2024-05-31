7bdf8c6086969bb692f80e740d04e076c9c076fb
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-05-31 17:55:53 +0700

    Add dropdown menu, fix url of OrderHistory and OrderDetail

--- FE/FE/Pages/Customer/Index.cshtml.cs
+++ FE/FE/Pages/Customer/Index.cshtml.cs
@@ -1,24 +1,33 @@
 using FE.Helpers;
 using FE.Models;
 using FE.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FE.Pages.Customer
 {
     public class IndexModel : PageModel
     {
         private readonly OrderService _orderService;
+
         public IndexModel(OrderService orderService)
         {
             _orderService = orderService;
         }
+
         public List<OrderHistory> OrderHistories { get; set; }
-        public async Task OnGetAsync()
+
+        public async Task<IActionResult> OnGetAsync(int? userId)
         {
             var account = SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user");
-            OrderHistories = await _orderService.GetAllOrderHistory(account.Id);
+
+            if (account == null || userId == null || account.Id != userId)
+            {
+                return RedirectToPage("/Login");
+            }
+
+            OrderHistories = await _orderService.GetAllOrderHistory(userId.Value);
+            return Page();
         }
     }
 }
