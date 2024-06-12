0f5e1b10e982b2a81802a4bf5dc8ad5697d001e2
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-06-13 01:27:01 +0700

    create a page for display detail of a address

---
+++ FE/FE/Pages/Customer/UserAddressDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FE.Pages.Customer
+{
+    public class UserAddressDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
