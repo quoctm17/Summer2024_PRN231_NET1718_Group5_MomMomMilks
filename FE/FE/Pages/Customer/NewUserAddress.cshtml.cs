b39c897c44d501d132f4be9f5316996b30b7defd
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-06-13 01:25:51 +0700

    Add NewUserAddress for creating address

---
+++ FE/FE/Pages/Customer/NewUserAddress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FE.Pages.Customer
+{
+    public class NewUserAddressModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
