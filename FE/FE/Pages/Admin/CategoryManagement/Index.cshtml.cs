b7afb7d68604ffb49df68a980e417ab304de38ca
tanutv123 <taitanvo2309@gmail.com>
2024-06-13 00:11:01 +0700

    Implement brand, category, milk age, supplier management for front-end

---
+++ FE/FE/Pages/Admin/CategoryManagement/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FE.Pages.Admin.CategoryManagement
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
