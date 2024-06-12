01eee95b4bc3d187b7123093c9587656a2c1390e
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-06-13 01:19:31 +0700

    Add phoneNumber for User model FE

--- FE/FE/Models/User.cs
+++ FE/FE/Models/User.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public byte Status { get; set; }
         public int Point { get; set; }
         public string Role { get; set; }
