60d34d4f779f4ec405c9f145c04659e4578e3aeb
nguyenphuongkiet <KietNPSE172931@fpt.edu.vn>
2024-06-13 17:22:00 +0700

    Setup cloudinary BE

---
+++ MomMomMilks/Service/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace Service.Interfaces
+{
+    public interface IImageService
+    {
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+        string GetImageUploadUrl(string publicId);
+        Task<ImageUploadResult> UploadAsync(IFormFile file);
+    }
+}
