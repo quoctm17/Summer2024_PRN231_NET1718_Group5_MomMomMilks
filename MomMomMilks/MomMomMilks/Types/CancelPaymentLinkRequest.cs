35976ec0ea58f96d2979dfd2ecd1675b6fb1df8a
quoctm17 <tranminhquoc0711@gmail.com>
2024-06-25 00:13:08 +0700

    Update Payment Controller. Add Cancel Payment Link

---
+++ MomMomMilks/MomMomMilks/Types/CancelPaymentLinkRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MomMomMilks.Types
+{
+    public class CancelPaymentLinkRequest
+    {
+        public long orderCode { get; set; }
+        public string? cancellationReason { get; set; }
+    }
+}
